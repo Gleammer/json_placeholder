cf8035a85f63de1c119f08e8fd4b4e8c3fe987d3
Radu Chirilov <radulin4a@gmail.com>
2021-02-21 20:18:07 +0200

    The JSON_Placeholder doesn't work when the db.json is longer than 10k characters, so I had to minify a bit the content, the indentation and the number of items.

--- db_creator.py
+++ db_creator.py
@@ -49,10 +49,6 @@ categories = [
 	{
 		'categories_id': 3,
 		'categories_title': 'Categoria 3'
-	},
-	{
-		'categories_id': 4,
-		'categories_title': 'Categoria 4'
 	}
 ]
 
@@ -67,9 +63,9 @@ def get_lesson_clip(index):
 	c_temp['clip_id'] = index
 	c_temp['clip_title'] = "Clip " + str(index)
 	c_temp['clip_thumbnail'] = "https://picsum.photos/id/" + str(index) + "/300/200"
-	c_temp['clip_description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+	c_temp['clip_description'] = "Desciption_cont."
 	c_temp['clip_categories'] = []
-	for x in range(randrange(4) + 1):
+	for x in range(randrange(3) + 1):
 		c_temp['clip_categories'].append(get_lesson_categories(x))
 	c_temp['clip_video_url'] = "https://youtu.be/dQw4w9WgXcQ"
 	return c_temp
@@ -78,7 +74,7 @@ def create_item(index):
 	temp = {}
 	temp['id'] = index
 	temp['thumbnail'] = "https://picsum.photos/id/" + str(index) + "/300/200"
-	temp['description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+	temp['description'] = "Desciption_cont."
 	if(randrange(2)):
 		temp['type'] = "post"
 		temp['metadata'] = get_post_meta()
@@ -91,9 +87,9 @@ def create_item(index):
 		temp['type'] = "lesson"
 		temp['video_url'] = "https://youtu.be/dQw4w9WgXcQ"
 		temp['categories'] = []
-		for x in range(randrange(4) + 1):
+		for x in range(randrange(3) + 1):
 			temp['categories'].append(get_lesson_categories(x))
-		temp['extern'] = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."			
+		temp['extern'] = "Extern_cont."			
 		temp['clips'] = []
 		for x in range(randrange(4)):
 			temp['clips'].append(get_lesson_clip(x))
@@ -103,14 +99,14 @@ def create_item(index):
 data = {}
 data['items'] = []
 
-for x in range(40):
+for x in range(20):
 	data['items'].append(create_item(x))
 
 ''' Useful for testing the outcome of db_cretor.py '''
 '''
 with open('testing_db.json', 'w') as outfile:
-	json.dump(data, outfile)
+	json.dump(data, outfile, sort_keys=True, indent=4)
 '''
 
 with open('db.json', 'w') as outfile:
-	json.dump(data, outfile)
+	json.dump(data, outfile, separators=(',',':'))
